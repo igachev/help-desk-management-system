702029c913b50eda56a215023ed274d26528a001
igachev <ivophoenix1135@gmail.com>
2024-09-08 00:15:26 +0300

    added pom dependencies
    I have setup the necessary dependencies

---
+++ help-desk-api/src/test/java/com/api/help_desk_api/HelpDeskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.help_desk_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDeskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
