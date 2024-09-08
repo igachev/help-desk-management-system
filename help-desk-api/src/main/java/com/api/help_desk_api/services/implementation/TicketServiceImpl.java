78c26efd5299f4649bc50b4adc7fcb6b37585a67
igachev <ivophoenix1135@gmail.com>
2024-09-08 15:57:44 +0300

    I implemented getAllTickets() in TicketServiceImpl

--- help-desk-api/src/main/java/com/api/help_desk_api/services/implementation/TicketServiceImpl.java
+++ help-desk-api/src/main/java/com/api/help_desk_api/services/implementation/TicketServiceImpl.java
@@ -1,12 +1,19 @@
 package com.api.help_desk_api.services.implementation;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.api.help_desk_api.dto.TicketDto;
+import com.api.help_desk_api.dto.TicketPaginationDto;
 import com.api.help_desk_api.models.Ticket;
 import com.api.help_desk_api.repositories.TicketRepository;
 import com.api.help_desk_api.services.TicketService;
+import java.util.stream.Collectors;
 
 @Service
 public class TicketServiceImpl implements TicketService {
@@ -33,6 +40,38 @@ public class TicketServiceImpl implements TicketService {
 
         return ticketDto;
     }
+
+    @Override
+    public TicketPaginationDto getAllTickets(int pageNo, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        Page<Ticket> tickets = ticketRepository.findAll(pageable);
+        System.out.println("tickets: " + tickets);
+        List<Ticket> listOfTickets = tickets.getContent();
+        System.out.println("listOfTickets: " + listOfTickets);
+
+        List<TicketDto> content = listOfTickets.stream()
+        .map((ticket) -> mapToDto(ticket)).collect(Collectors.toList());
+        System.out.println("content: " + content);
+
+        TicketPaginationDto ticketPaginationDto = new TicketPaginationDto();
+        ticketPaginationDto.setContent(content);
+        ticketPaginationDto.setPageNo(tickets.getNumber());
+        ticketPaginationDto.setPageSize(tickets.getSize());
+        ticketPaginationDto.setTotalElements(tickets.getTotalElements());
+        ticketPaginationDto.setTotalPages(tickets.getTotalPages());
+        ticketPaginationDto.setLast(tickets.isLast());
+
+        return ticketPaginationDto;
+    }
     
     
+    private TicketDto mapToDto(Ticket ticket) {
+        TicketDto ticketDto = new TicketDto();
+        ticketDto.setId(ticket.getId());
+        ticketDto.setTicketTitle(ticket.getTicketTitle());
+        ticketDto.setTicketDescription(ticket.getTicketDescription());
+        ticketDto.setLocalDateTime(ticket.getLocalDateTime());
+        return ticketDto;
+    }
+
 }
