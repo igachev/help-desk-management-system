903b2af4fd7501522564e3ff20cc823da5b3f9c2
igachev <ivophoenix1135@gmail.com>
2024-09-08 15:57:05 +0300

    I added getAllTickets() to TicketService Interface

--- help-desk-api/src/main/java/com/api/help_desk_api/services/TicketService.java
+++ help-desk-api/src/main/java/com/api/help_desk_api/services/TicketService.java
@@ -1,7 +1,9 @@
 package com.api.help_desk_api.services;
 
 import com.api.help_desk_api.dto.TicketDto;
+import com.api.help_desk_api.dto.TicketPaginationDto;
 
 public interface TicketService {
     TicketDto createTicket(TicketDto ticketDto);
+    TicketPaginationDto getAllTickets(int pageNo, int pageSize);
 }
