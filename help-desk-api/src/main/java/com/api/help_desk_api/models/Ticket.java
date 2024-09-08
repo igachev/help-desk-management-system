024c72a3976f15380e9fe4149e46727ab9ee42fd
igachev <ivophoenix1135@gmail.com>
2024-09-08 13:09:58 +0300

    I added Ticket model

---
+++ help-desk-api/src/main/java/com/api/help_desk_api/models/Ticket.java
@@ -0,0 +1,74 @@
+package com.api.help_desk_api.models;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Ticket {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "ticket_title")
+    private String ticketTitle;
+    @Column(name = "ticket_description")
+    private String ticketDescription;
+    @CreatedDate
+    @Column(name = "local_date_time")
+    private Date localDateTime;
+
+    public Ticket() { }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTicketTitle() {
+        return ticketTitle;
+    }
+
+    public void setTicketTitle(String ticketTitle) {
+        this.ticketTitle = ticketTitle;
+    }
+
+    public String getTicketDescription() {
+        return ticketDescription;
+    }
+
+    public void setTicketDescription(String ticketDescription) {
+        this.ticketDescription = ticketDescription;
+    }
+
+    public Date getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public void setLocalDateTime(Date localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket [id=" + id + ", ticketTitle=" + ticketTitle + ", ticketDescription=" + ticketDescription
+                + ", localDateTime=" + localDateTime + "]";
+    }
+
+    
+
+    
+}
