e2f20df6d981ddb86d4c06fca66c3d1aa8d54756
igachev <ivophoenix1135@gmail.com>
2024-09-21 16:39:15 +0300

    added route /login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import TicketsView from './features/ticket/TicketsView/TicketsView'
 import TicketView from './features/ticket/TicketView/TicketView'
+import LoginView from './features/user/LoginView/LoginView'
 
 
 
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path='/' element={<TicketsView />} />
         <Route path='tickets/:ticketId' element={<TicketView />} />
+        <Route path='login' element={<LoginView />} />
       </Routes>
     </main>
     <footer>Footer</footer>
