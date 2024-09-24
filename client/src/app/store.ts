c424d8c538621229ff465dc350eb7e6313d501fb
igachev <ivophoenix1135@gmail.com>
2024-09-24 12:24:51 +0300

    Installed redux-persist
    I implemented redux-persist in order to store the redux state in the local storage

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,17 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import ticketReducer from "../features/ticket/ticketSlice";
-import userReducer from "../features/user/userSlice" 
+import userReducer from "../features/user/userSlice"
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+
+const rootReducer = combineReducers({ 
+    ticket: ticketReducer,
+    user: userReducer,
+  })
+
+const persistedReducer = persistReducer(persistConfig,rootReducer)
+  
 const store = configureStore({
-    reducer: {
-        ticket: ticketReducer,
-        user: userReducer
-    },
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false
     })
 })
 
 export default store
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,14 +4,17 @@ import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import store from './app/store.ts'
+import store, { persistor } from './app/store.ts'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
   <BrowserRouter>
+  <PersistGate loading={null} persistor={persistor}>
   <StrictMode>
     <App />
   </StrictMode>
+  </PersistGate>
   </BrowserRouter>
   </Provider>
 )
