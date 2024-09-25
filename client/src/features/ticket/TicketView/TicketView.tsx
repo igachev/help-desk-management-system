83dca8332bdbf07b22b890912cea5ded4979fc8e
igachev <ivophoenix1135@gmail.com>
2024-09-25 13:18:15 +0300

    I updated TicketView component
    1) I created onResolveTicket function
    2) I added button Resolve Ticket that is visible only for admin user
    3) I edited useEffect to trigger when resolved variable changes its value

--- client/src/features/ticket/TicketView/TicketView.tsx
+++ client/src/features/ticket/TicketView/TicketView.tsx
@@ -1,8 +1,9 @@
 import { useDispatch, useSelector } from "react-redux"
 import { Link, useNavigate, useParams } from "react-router-dom"
 import { RootState } from "../../../app/store"
-import { useEffect } from "react"
-import { deleteTicket, fetchTicket } from "../ticketSlice"
+import { useEffect, useState } from "react"
+import { deleteTicket, fetchTicket, resolveTicket } from "../ticketSlice"
+import { getCookie } from "../../../cookie"
 
 
 const TicketView = () => {
@@ -13,13 +14,21 @@ const ticket = useSelector((state: RootState) => state.ticket)
 const user = useSelector((state: RootState) => state.user)
 const dispatch = useDispatch()
 const userId = user.data.userId
+const userEmail = getCookie("userEmail")
+const decodedUserEmail = decodeURIComponent(userEmail || '')
+let isAdmin = decodedUserEmail === 'admin@abv.bg' ? true : false
+const [resolved,setResolved] = useState<boolean>(ticket.data.ticket?.resolved) 
 
 useEffect(() => {
     dispatch(fetchTicket({ticketId}))
-},[])
+},[resolved])
 
 function onDeleteTicket() {
     dispatch(deleteTicket({userId,ticketId,navigation}))
+}
+
+function onResolveTicket() {
+    dispatch(resolveTicket({userId,ticketId,setResolved}))
 }
 
   return (
@@ -42,6 +51,7 @@ function onDeleteTicket() {
             <h2>Ticket Title: {ticket.data.ticket.ticketTitle}</h2>
             <h3>Created At: {ticket.data.ticket.createdAt?.toString()}</h3>
             <p>Ticket Description: {ticket.data.ticket.ticketDescription}</p>
+            <p>Problem Fixed: {ticket.data.ticket.resolved ? "true" : "false"}</p>
             </div>
         )}
 
@@ -49,6 +59,12 @@ function onDeleteTicket() {
                 <div>
                     <Link to={`/tickets/${ticketId}/edit`}>Edit</Link>
                     <button onClick={onDeleteTicket}>Delete</button>
+                </div>
+            )}
+
+            {isAdmin && (
+                <div>
+                    <button onClick={onResolveTicket}>Resolve Ticket</button>
                 </div>
             )}
 
