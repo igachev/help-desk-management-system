02e9da0a381de60105fcb242f023e12a003ea726
igachev <ivophoenix1135@gmail.com>
2024-09-27 15:08:34 +0300

    I added register link in the navigation menu available only for logged out users

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -19,6 +19,7 @@ const Header = () => {
                 {!accessToken && (
                     <>
                     <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/register">Register</Link></li>
                     </>
                 )}
 
